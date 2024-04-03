9870a1b64f4c8b077122fe9303b3fec3db48e755
akazicin <akazicin@gmail.com>
2024-04-03 23:59:29 +0300

    Homework Massiv

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,84 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //task1
+
+        int[] weight = new int[3];
+        for (int i = 0; i < weight.length; i++) {
+            weight[i] = i + 1;
+
+        }
+        double[] nums = {1.57, 7.654, 9.986};
+        boolean[] booleans = {true, false, true};
+
+        ///task2
+
+        for (int i = 0; i < weight.length; i++) {
+            weight[i] = i + 1;
+            System.out.print(weight[i]);
+            if (i < weight.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i]);
+            if (i < nums.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        for (int i = 0; i < booleans.length; i++) {
+            System.out.print(booleans[i]);
+            if (i < booleans.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+        ///task3
+        for ( int i = weight.length - 1; i >= 0; i--){
+            System.out.print(weight [i]);
+            if (i > 0 ){
+                System.out.print( ", ");
+            }
+
+        }
+        System.out.println();
+
+        for (int i = nums.length -1 ; i >=0 ; i--) {
+            System.out.print(nums [i]);
+            if (i > 0) {
+                System.out.print(", ");
+
+            }
+        }
+        System.out.println();
+
+        for (int i = booleans.length -1; i >=0 ; i--) {
+            System.out.print( booleans [i]);
+        if (i > 0); {
+                System.out.print( ", ");
+            }
+        }
+
+        System.out.println();
+
+        ///task4
+
+        for (int i = 0; i < weight.length; i++) {
+            if (weight[i] % 2 !=0 ){
+                weight [i] ++;
+            }
+        }
+        for (int i = 0; i < weight.length; i++) {
+            System.out.print( weight[i]);
+            if (i<weight.length - 1) {
+                System.out.print( ", ");
+            }
+            
+        }
+    
     }
 }
