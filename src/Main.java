009c40f490d8bd2033f5aff040fdbf505c9da263
akazicin <akazicin@gmail.com>
2024-04-04 20:31:02 +0300

    Homework Massiv

--- src/Main.java
+++ src/Main.java
@@ -16,9 +16,9 @@ public class Main {
         for (int i = 0; i < weight.length; i++) {
             weight[i] = i + 1;
             System.out.print(weight[i]);
-            if (i < weight.length - 1) {
+            if (i < weight.length - 1)
                 System.out.print(", ");
-            }
+
         }
         System.out.println();
         for (int i = 0; i < nums.length; i++) {
@@ -58,7 +58,7 @@ public class Main {
 
         for (int i = booleans.length -1; i >=0 ; i--) {
             System.out.print( booleans [i]);
-        if (i > 0); {
+            if (i > 0) {
                 System.out.print( ", ");
             }
         }
